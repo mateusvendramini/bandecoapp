206733ec4f5127da1b3255090e93b1a7782e7318
Ricardo Imagure <ricardo.imagure@usp.br>
2017-11-11 21:31:36 -0200

    adicionando restrições para inserção de info

--- app/models/restaurante.rb
+++ app/models/restaurante.rb
@@ -1,2 +1,16 @@
 class Restaurante < ApplicationRecord
+	validates :nome, presence: true
+	validates :nome, uniqueness: true
+
+	validates :cardapio, presence: true
+	validates :cardapio, uniqueness: true
+
+	validates :layout, presence: true
+	validates :layout, uniqueness: true
+	
+	validates :mapa, presence: true
+	validates :mapa, uniqueness: true
+	
+	validates :foto, presence: true
+	validates :foto, uniqueness: true
 end
