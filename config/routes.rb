ce5c0bcac7c4ca54524f0642bd794243640b973c
Ricardo Imagure <ricardo.imagure@usp.br>
2017-11-12 03:18:32 -0200

    modificando imagem da fila do central

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   patch 'restaurantes/update'
   post 'restaurantes/update'
   get 'restaurantes/delete'
+  get 'restaurantes/confirm'
+  get 'restaurantes/data'
   get  'restaurantes/layout_central'
   get 'restaurantes/layout_fisica'
   get 'restaurantes/img_reserva'
