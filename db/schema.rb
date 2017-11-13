775e351c89a2e1000aa868e05a7878f9d06174dd
Ricardo Imagure <ricardo.imagure@usp.br>
2017-11-13 15:09:08 -0200

    coisas do merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171108011804) do
-
+ActiveRecord::Schema.define(version: 20171110194304) do
 
   create_table "admins", force: :cascade do |t|
     t.string "login"
