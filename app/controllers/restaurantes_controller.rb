1a7a61c17ca57e1a895ace406d4d8fece33dad02
Ricardo Imagure <ricardo.imagure@usp.br>
2017-11-14 20:15:51 -0200

    mudanças inúteis

--- app/controllers/restaurantes_controller.rb
+++ app/controllers/restaurantes_controller.rb
@@ -77,6 +77,8 @@ class RestaurantesController < ApplicationController
   def img_reserva
   end
 
+  
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_restaurante
